dc465a7cc6d37bc6226315b1a3f1bfba0ff9016e
GavinGeier <gavingeier22@gmail.com>
2024-08-14 00:04:04 -0400

    add get_char_dict function which returns the number of times each character appears in the string.

--- main.py
+++ main.py
@@ -3,11 +3,26 @@ def main():
     text = get_book_text(book_path)
     num_words = get_word_count(text)
     print(f"{num_words} words found in the document.")
+    char_dict = get_char_dict(text)
+    print(f"Number of times each character appears in the text: {char_dict}")
+    
 
-def word_count(text):
+
+
+def get_word_count(text):
     words = text.split()
     return len(words)
 
+def get_char_dict(text):
+    char_dict = {}
+
+    for i in text:
+        lowered = i.lower()
+        if lowered.isalpha() and lowered not in char_dict:
+            char_dict[lowered] = 1
+        elif lowered.isalpha():
+            char_dict[lowered] += 1
+    return char_dict
 
 
 
